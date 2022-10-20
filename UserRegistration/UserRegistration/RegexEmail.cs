e986803f55810eba710f85b731935867f646bf4d
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-20 16:48:10 +0530

    Enter Valid Email

--- UserRegistration/UserRegistration/RegexEmail.cs
+++ UserRegistration/UserRegistration/RegexEmail.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace UserRegistration
 {
-    internal class RegexEmail
+    public class RegexEmail
     {
+        public static string Email = "^[a-zA-Z]+([@#%$&+.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+
+        public bool validateString(string Name)
+        {
+            return Regex.IsMatch(Name, Email);
+        }
     }
 }

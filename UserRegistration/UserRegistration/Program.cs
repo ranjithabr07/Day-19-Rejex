bcb8b2b292ccfef78dbc7b149a1ae9e0ca91e85a
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-20 16:11:42 +0530

    Enter Valid First Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using UserRegistration;
+
+Console.WriteLine("Welcome to User Registration Problem");
+
+RegexFirstName firstName = new RegexFirstName();
+
+if (firstName.validateString("Ranjitha"))
+
+{
+    Console.WriteLine("The Name is Valid");
+
+}
+else
+{
+    Console.WriteLine("The Name is InValid");
+}
---
+++ UserRegistration/UserRegistration/RegexFirstName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class RegexFirstName
+    {
+        public static string FirstName = "^[A-Z][a-z]{2,}$";
+        public bool validateString(string Name)
+        {
+            return Regex.IsMatch(Name, FirstName);
+        }
+    }
+}
+}

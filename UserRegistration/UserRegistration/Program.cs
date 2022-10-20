62260ea46dd10b9e252aa6ac3f0c8fe16a445884
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-20 16:17:00 +0530

    Enter Valid Last Name

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,18 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using UserRegistration;
+﻿using UserRegistration;
 
 Console.WriteLine("Welcome to User Registration Problem");
+Console.WriteLine("Enter 1 for First Name");
+Console.WriteLine("Enter 2 for Last Name");
 
-RegexFirstName firstName = new RegexFirstName();
+int num = Convert.ToInt32(Console.ReadLine());
 
-if (firstName.validateString("Ranjitha"))
-
+switch (num)
 {
-    Console.WriteLine("The Name is Valid");
+    case 1:
+        RegexFirstName firstName = new RegexFirstName();
+        bool ValidName = firstName.validateString("Madhu");
+        Console.WriteLine("{0}", (ValidName) ? "Valid" : "Invalid");
+        break;
 
-}
-else
-{
-    Console.WriteLine("The Name is InValid");
+    case 2:
+        RegexLastName lastName = new RegexLastName();
+        bool ValidName2 = lastName.validateString("Shivakumar");
+        Console.WriteLine("{0}", (ValidName2) ? "Valid" : "Invalid");
+        break;
 }

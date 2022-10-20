dae978e5385137d4010367a7d1fc00be89636920
ranjithabr07 <ranjitharanjubrhlk1998@gmail.com>
2022-10-20 17:15:47 +0530

    Check the all the emails samples

--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("Enter 5 for PassWord Rule1");
 Console.WriteLine("Enter 6 for PassWord Rule2");
 Console.WriteLine("Enter 7 for PassWord Rule3");
 Console.WriteLine("Enter 8 for PassWord Rule4");
-
+Console.WriteLine("Enter 9 for Email samples");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -62,4 +62,28 @@ switch (num)
         bool PassWord4 = regexPassWordRule4.validateString("ABJHFTDfg67@@");
         Console.WriteLine("{0}", (PassWord4) ? "Valid" : "Invalid");
         break;
+
+    case 9:
+        RegexEmailSample emailSample = new RegexEmailSample();
+        List<string> validtests = new List<string>() {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" ,
+                                                     "abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
+
+        foreach (var test in validtests)
+        {
+            bool result = emailSample.validateString(test);
+            if (result)
+            {
+                Console.WriteLine("\n{0} is a valid email address", test);
+            }
+            else
+            {
+                Console.WriteLine("\n{0} is an invalid email address", test);
+            }
+        }
+        break;
+
+    default:
+        Console.WriteLine("Enter a  valid number");
+        break;
+
 }
---
+++ UserRegistration/UserRegistration/RegexEmailSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class RegexEmailSample
+    {
+        public static string EmailSample = "^[a-zA-Z0-9]+([@#%$&+.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,})?$";
+
+        public bool validateString(string Name)
+        {
+            return Regex.IsMatch(Name, EmailSample);
+        }
+    }
+}
